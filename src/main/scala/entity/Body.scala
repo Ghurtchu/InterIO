cf8c3191a32e5fd4d128a756dc7acc46d765d9e7
nghurtch <nghurtch@broadinstitute.com>
2022-05-12 21:28:57 +0400

    add extension fold method to tuple of two options of strings

--- src/main/scala/entity/Body.scala
+++ src/main/scala/entity/Body.scala
@@ -5,6 +5,8 @@ import entity.ContentType.*
 enum Body(val get: String, val contentType: ContentType):
 
   case html(override val get: String) extends Body(get, HTML)
+    def +(other: html): html = html(get concat "\n" concat other.get)
+      
   case json(override val get: String) extends Body(get, JSON)
   case xml(override val get: String) extends Body(get, XML)
   case csv(override val get: String) extends Body(get, CSV)
--- src/main/scala/handler/ProductHandler.scala
+++ src/main/scala/handler/ProductHandler.scala
@@ -1,20 +1,32 @@
 package handler
 
 import entity.Body.html
+import handler.ProductHandler.{defaultPrice, defaultProduct}
 import request.{HttpRequest, HttpRequestHandler, HttpResponse, HttpResponseWriter}
+
+import scala.util.Try
 
 
 class ProductHandler(val httpRequest: HttpRequest, override val writer: HttpResponseWriter) extends HttpRequestHandler(httpRequest, writer) :
 
-  lazy val defaultProduct = "Banana"
-  lazy val defaultPrice = 1.0
-
   override def handle(): Unit =
-    val prodName = httpRequest.queryParams.get("productName").fold(defaultProduct)(identity)
-    val price = httpRequest.queryParams.get("price").fold(defaultPrice)(identity)
-    val body = html(
-      s"<h1> Product name = $prodName </h1>"
-        .concat(s"\nProduct price = $price")
-    )
+    val queryParams = httpRequest.queryParams
+    val maybeProdName = queryParams get "productName"
+    val maybeProdPrice = queryParams get "productPrice"
+    val (prodName, prodPrice) = (maybeProdName, maybeProdPrice).fold(defaultProduct, defaultPrice)((a, b) => (a, b))
+    val body = html(s"<h1> Request has been processed successfully")
+      + html(s"<h2> Result: </h2>")
+      + html(s"<h3> Requested product name => $prodName </h3>")
+      + html(s"<h3> Requested product price => $prodPrice </h3>")
     val response = HttpResponse(body)
     writer.write(response)
+
+object ProductHandler:
+  lazy val defaultProduct = "Scala T shirt"
+  lazy val defaultPrice = "20"
+
+extension(tupleOfOptionStrings: (Option[String], Option[String]))
+  def fold(ifEmpty: => (String, String))(f: (String, String) => (String, String)): (String, String) =
+    tupleOfOptionStrings match
+      case (Some(first), Some(second)) => f(first, second)
+      case _ => ifEmpty
